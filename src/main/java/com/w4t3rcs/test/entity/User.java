f1c930fad38ac68c9affa0ccd3da9502f59fa548
w4t3rcs <w4t3rofficial@gmail.com>
2024-06-18 10:41:25 +0200

    Keycloak + Spring Security + Moved from PostgreSQL to MySQL

--- src/main/java/com/w4t3rcs/test/entity/User.java
+++ src/main/java/com/w4t3rcs/test/entity/User.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     @NotBlank
     @Column(unique = true)
