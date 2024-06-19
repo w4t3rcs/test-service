e7fb5c648776676458579fcf272f6fb12207b1a8
w4t3rcs <w4t3rofficial@gmail.com>
2024-06-19 11:29:11 +0200

    Fixed response statuses for UserNotFoundException and ArticleNotFoundException
    Response status' code was 400 (HttpStatus.BAD_REQUEST), but it should be 404 (HttpStatus.NOT_FOUND)

--- src/main/java/com/w4t3rcs/test/exception/ArticleNotFoundException.java
+++ src/main/java/com/w4t3rcs/test/exception/ArticleNotFoundException.java
@@ -3,6 +3,6 @@ package com.w4t3rcs.test.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Article hasn't been found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Article hasn't been found")
 public class ArticleNotFoundException extends RuntimeException {
 }
--- src/main/java/com/w4t3rcs/test/exception/UserNotFoundException.java
+++ src/main/java/com/w4t3rcs/test/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.w4t3rcs.test.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "User hasn't been found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User hasn't been found")
 public class UserNotFoundException extends RuntimeException {
 }
